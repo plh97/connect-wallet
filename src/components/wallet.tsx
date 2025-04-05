43f43647e707c68987b0c69809ddd70001caa837
plh97 <pengliheng111@gmail.com>
2025-04-05 17:05:20 +0800

    feat: init transfer wallet

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -12,7 +12,6 @@ export function Wallet() {
         ethBalance,
         wethBalance,
         txStatus,
-        switchNetwork,
     } = useMetaMask();
     const [transferAmount, setTransferAmount] = useState("0.0000001");
     const [receiverAddress, setReceiverAddress] = useState(
@@ -35,9 +34,6 @@ export function Wallet() {
                     <button>已经连接钱包</button>
                     <button onClick={refreshBalance}>
                         刷新余额
-                    </button>
-                    <button onClick={switchNetwork}>
-                        switchNetwork
                     </button>
                 </>
             ) : (
